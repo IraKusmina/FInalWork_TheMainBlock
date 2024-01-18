c4658af780534a326f5ecd0505d3a48093ad343f
Ira <irakusmina@mail.ru>
2024-01-18 20:38:02 +0300

    Добавление папки Task4 для проверки работы кода

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+string [] massiv = ["1234", "1567", "-2", "computer science"];
+string [] array = new string[massiv.Length]; 
+int j = 0;
+for (int i = 0; i < massiv.Length; i++){
+    if (massiv[i].Length <= 3){
+        array[j] = massiv[i];
+        j++;
+    } 
+}
+Console.WriteLine ($"Начальный массив: {string.Join(" ", massiv)}");
+Console.WriteLine ($"Конечный массив: {string.Join(" ", array)}");
